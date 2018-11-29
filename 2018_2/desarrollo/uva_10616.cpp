097b806d0a5612e28d553ce48196fdc4f6ea28a8
Esteban <estebanibaca@udec.cl>
2018-11-29 13:53:05 -0300

    no entregado 10616

---
+++ 2018_2/desarrollo/uva_10616.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+
+  int set =1;
+  int N;
+  int Q;
+  vector<int> numbers;
+  cin >> N;
+  cin >> Q
+
+  while (N!=0 && Q !=0) {
+
+  for (int i = 0; i < N; i++) {
+    int temp;
+    cin >> temp;
+    numbers.push_back(temp);
+  }
+
+  for (int i = 0; i < Q; i++) {
+    int querie;
+    int D,M;
+    cin >> querie;
+  }
+
+
+  }
+
+
+
+  return 0;
+}

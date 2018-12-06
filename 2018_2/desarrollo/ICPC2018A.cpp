807c0ab30d1c26827e2fac0cf58069c34a5cbb9a
Esteban <estebanibaca@udec.cl>
2018-12-06 19:02:56 -0300

    ICPCA corresponde casos de prueba

--- 2018_2/desarrollo/ICPC2018A.cpp
+++ 2018_2/desarrollo/ICPC2018A.cpp
@@ -1,72 +1,24 @@
-  #include <bits/stdc++.h>
+#include <bits/stdc++.h>
+/*Idea del codigo extraida de https://codeforces.com/blog/entry/63157*/
 
-  using namespace std;
+using namespace std;
 
-
-  string clearString(string input){
-    string output = input;
-
-    //Remover puntos
-    //Remover todo lo que este despues del + hasta antes del @
-
-    auto it = output.begin();
-
-    while (it != output.end()) {
-      auto currentChar = *it;
-
-      if(currentChar == '.'){
-        cout << " Encontrado ."<<endl;
-        cout << " Output =" << output << endl;
-        output.erase(it);
-      }
-      else if(currentChar == '+'){
-        cout << " Encontrado +"<<endl;
-        auto arrobaPos =  find(output.begin(),output.end(),'@');
-        //cout << "First to erase "<< it << endl;
-        //cout << "Last to erase "<< arrobaPos<<endl;
-
-        output.erase(it,arrobaPos);
-        cout << " Output =" << output << endl;
+int main(){
+  float r;
+  while (cin >> r) {
+    int i=1;
+    while (true) {
+      if(fmod(i*r,360)==0){
+        cout << i << endl;
         break;
       }
-else if(currentChar == '@') break;
       else{
-        cout << " Encontrado letra normal: "<<currentChar<<endl;
-        ++it;
+        i++;
       }
     }
-    return output;
-}
-
-  int main (){
-
-  int sizeDbEmail=0;
-
-  set <string>uniqueEmails;
-
-  vector<string> listEmails;
-
-  while(cin >> sizeDbEmail){
-    cout << "Db Size :"<<sizeDbEmail<<endl;
-    for (int i = 0; i < sizeDbEmail; i++) {
-      string temp;
-      cin >> temp;
-      cout << temp <<endl;
-
-      temp = clearString(temp);
-      cout << "Email cleared: "<< temp<<endl;
-      uniqueEmails.insert(temp);
-    }
-  cout << uniqueEmails.size()<<endl;
-  uniqueEmails.clear();
-
-  //listEmails.clear();
-
-
   }
-
 
 
 
   return 0;
-  }
+}
---
+++ 2018_2/desarrollo/ICPC2018B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int nTrees;
+  vector<int> trees;
+  vector<float> treeAngles;
+
+  while (cin >> nTrees) {
+    float length =0;
+    trees.push_back(0);
+
+    for (int i = 1; i < nTrees+1; i++) {
+      int auxTree;
+      cin >> auxTree;
+      trees.push_back(auxTree);
+
+      length += auxTree;
+    }
+    float initialAngle=0;
+    cout << "Total length: "<<length<<endl;
+    for (int i = 0; i < nTrees+1; i++) {
+
+      if(i==0)
+      treeAngles.push_back(trees[i] * (360/length));
+      else{
+        treeAngles.push_back(treeAngles[i-1]+trees[i] * (360/length));
+      }
+
+
+      cout << "Tree "<<i<<" .Distance recorred: "<<trees[i]<<". Angle :"<<treeAngles[i]<<endl;
+    }
+
+    cout << endl;
+    trees.clear();
+  }
+
+
+
+  return 0;
+}
